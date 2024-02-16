c0c2f9718b3cd571a804da798f5e043ffc13a29f
Praveen Singh Rawat <144586433+PraveenSinghrwt@users.noreply.github.com>
2024-02-16 23:52:18 +0530

    Update Blog.jsx

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = () => {
             <div className=" bg-white shadow-md rounded-lg sm:m-8 text-center">
                 <h2 className="text-xl lg:text-2xl font-bold text-gray-800 mt-4 mb-4">{title}</h2>
                 <div className=' flex justify-center items-center mb-2'>
-                    <img className='w-[90%] h-60 md:w-[70%] md:h-80 lg:w-[60%] xl:w-[50%] shadow-xl shadow-gray-200' src={`/Images/${image}`} alt='' />
+                    <img className='w-[90%] h-60 md:w-[70%] md:h-80 lg:w-[60%] xl:w-[50%] shadow-xl shadow-gray-200' src={`https://blogger-server-api.onrender.com/Images/${image}`} alt='' />
                 </div>
                 <div className="p-4">
                     <p className=" mb-4 text-sm text-gray-600 text-start">By {author} on  {new Date(createdDate).toLocaleDateString()}</p>
